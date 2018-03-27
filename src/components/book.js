20a03dde5362da16a1a299fac4db5492694953dd
MargaritaKhodzka <mkhodzka@gmail.com>
2018-03-27 00:01:49 -0400

    built book model component

---
+++ src/components/book.js
@@ -0,0 +1,14 @@
+class Book {
+  constructor(bookJSON) {
+    this.title = bookJSON.title
+    this.id = bookJSON.id
+  }
+
+  render() {
+    return `<li data-bookid='${this.id}' data-props='${JSON.stringify(
+      this
+    )}' class='book-element'><a class="show-link" href='#'>${
+      this.title
+    }</a><button data-action='edit-book'>Edit</button><i data-action='delete-book' class="em em-scream_cat"></i></li>`
+  }
+}
