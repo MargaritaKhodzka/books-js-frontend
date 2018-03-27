cd45a01ef0a547ce21ae66e4d89a333b392aa8d5
MargaritaKhodzka <mkhodzka@gmail.com>
2018-03-27 00:35:28 -0400

    added show method to Book component

--- src/components/book.js
+++ src/components/book.js
@@ -4,11 +4,15 @@ class Book {
     this.id = bookJSON.id
   }
 
+  renderShow() {
+    return `<h3>${this.title}</h3>`
+  }
+
   render() {
     return `<li data-bookid='${this.id}' data-props='${JSON.stringify(
       this
     )}' class='book-element'><a class="show-link" href='#'>${
       this.title
-    }</a><button data-action='edit-book'>Edit</button><i data-action='delete-book' class="em em-scream_cat"></i></li>`
+    }</a><button data-action='edit-book'>Edit</button> <i data-action='delete-book' class="em em-scream_cat"></i></li>`
   }
 }
